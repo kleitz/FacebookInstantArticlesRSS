a751da75407a779e198f46dd7697fa9e165d31fc
Patrick Wilver <pwilver@hubspot.com>
2016-03-24 18:29:52 -0400

    Add method declarations for routes

--- routes/articles.js
+++ routes/articles.js
@@ -5,31 +5,29 @@ var cors = require('cors');
 
 var Q = require('q');
 
-var pg = require('pg').native;
-var PG_URL = process.env.DATABASE_URL || 'postgres://pwilver:@localhost/rssfeed';
-var pgClient = new pg.Client(PG_URL);
+// var pg = require('pg').native;
+// var PG_URL = process.env.DATABASE_URL || 'postgres://pwilver:@localhost/rssfeed';
+// var pgClient = new pg.Client(PG_URL);
 
-pgClient.connect(function(err) {
-	if (err) {
-		return console.error('could not connect to postgres', err);
-	} else {
-		pgClient.query("CREATE TABLE IF NOT EXISTS test(number integer,value text)", function(err, result) {
-			if (err) { 
-				return console.error('error creating table', err ); 
-			}
-		});
+// pgClient.connect(function(err) {
+// 	if (err) {
+// 		return console.error('could not connect to postgres', err);
+// 	} else {
+// 		pgClient.query("CREATE TABLE IF NOT EXISTS test(number integer,value text)", function(err, result) {
+// 			if (err) { 
+// 				return console.error('error creating table', err ); 
+// 			}
+// 		});
 
-		return console.log('Connected to postgres!');
-	}
-});
+// 		return console.log('Connected to postgres!');
+// 	}
+// });
 
+// POST blog data to db
+// =====================
 
-// R O U T E
-// =========
-router.get('/article/new', function() {
+router.post('/submit', function(req, res) {
 	// 
 });
 
-router.post('/article/submit', function(req, res) {
-	// 
-});
+module.exports = router;
---
+++ routes/rss-feed.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+// Retrieve data from database
+// Parse it in XML format
+// Show it on page (EJS)
+
+
+// GET rss feed contents
+// ======================
+
+router.get('/', function(req, res) {
+	// 
+});
