538b2084e7a227a59d80b745a0c733855641f47e
Patrick Wilver <pwilver@hubspot.com>
2016-03-28 17:31:52 -0400

    Configure proper table column names for initial table setup in db

--- routes/articles.js
+++ routes/articles.js
@@ -5,23 +5,25 @@ var cors = require('cors');
 
 var Q = require('q');
 
-// var pg = require('pg').native;
-// var PG_URL = process.env.DATABASE_URL || 'postgres://pwilver:@localhost/rssfeed';
-// var pgClient = new pg.Client(PG_URL);
+var pg = require('pg').native;
+var PG_URL = process.env.DATABASE_URL || 'postgres://pwilver:@localhost/facebookIA';
+var pgClient = new pg.Client(PG_URL);
 
-// pgClient.connect(function(err) {
-// 	if (err) {
-// 		return console.error('could not connect to postgres', err);
-// 	} else {
-// 		pgClient.query("CREATE TABLE IF NOT EXISTS test(number integer,value text)", function(err, result) {
-// 			if (err) { 
-// 				return console.error('error creating table', err ); 
-// 			}
-// 		});
+pgClient.connect(function(err) {
+	if (err) {
+		return console.error('could not connect to postgres', err);
+	} else {
+		pgClient.query("CREATE TABLE IF NOT EXISTS rssItems(id serial PRIMARY KEY NOT NULL,title text,canonical_url text,guid bigint,meta_description text,publish_date bigint,author text)", function(err, result) {
+			if (err) { 
+				return console.error('error creating table', err ); 
+			}
+		});
 
-// 		return console.log('Connected to postgres!');
-// 	}
-// });
+		return console.log('Connected to postgres!');
+	}
+});
+
+
 
 // POST blog data to db
 // =====================
