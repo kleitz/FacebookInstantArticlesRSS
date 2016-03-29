eb789962bfd4122d53f8535fafaaa4b7206e59d1
Patrick Wilver <pwilver@hubspot.com>
2016-03-29 17:10:56 -0400

    Add functionality for connecting to postgres and pulling blog data by given id

--- routes/articles.js
+++ routes/articles.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
-var cors = require('cors');
-
+var request = require('request');
 var Q = require('q');
 
+var functions = require('../apiFunctions');
+
 var pg = require('pg').native;
-var PG_URL = process.env.DATABASE_URL || 'postgres://pwilver:@localhost/facebookIA';
+var PG_URL = process.env.DATABASE_URL || 'postgres://pwilver:@localhost/facebookia';
 var pgClient = new pg.Client(PG_URL);
 
+
+// Connect to db
 pgClient.connect(function(err) {
 	if (err) {
 		return console.error('could not connect to postgres', err);
@@ -25,11 +27,27 @@ pgClient.connect(function(err) {
 
 
 
+
 // POST blog data to db
 // =====================
-
 router.post('/submit', function(req, res) {
-	// 
+	var id = req.param('blog_id'),
+		postData;
+
+	functions.fetchBlogData(id).then(function(data) {
+		postData = data.data[0];
+	});
+
+	var title = postData.name,
+		canonical_url = postData.url,
+		meta_description = postData.meta_description,
+		publish_date = postData.publish_date,
+		author = postData.blog_author.full_name;
+
+	// 	functions.postNewArticle(title, canonical_url, id, meta_description, publish_date, author).then(function(response) {
+	// 		res.status(response.status).json(response);
+	// 	});
+	// });
 });
 
 module.exports = router;
