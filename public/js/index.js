d4eb9fa2c39ea4168a9612caf3b152e5cadfd39b
Patrick Wilver <pwilver@hubspot.com>
2016-04-05 16:14:09 -0400

    Configure front-end UI behavior for successful and failing blog API requests

--- public/js/index.js
+++ public/js/index.js
@@ -13,20 +13,41 @@ var app = {
 			});
 		},
 
-		postBlogContents: function(post) {
+		postBlogContents: function() {
 			var $form = $('.hs-form');
+
+			// Remove any error messages on page
+			$('.submit--message').html('&nbsp;');
 
 			// Change submit button text to indicate process
 			$('.post__submit').val('Submitting...');
 
 			$.post('//localhost:8080/article/submit?' + $form.serialize())
 				.done(function(data) {
-					console.log(data.message);
 					$('.post__submit').val('Posted!');
+
+					if ($('.submit--message').length < 1) {
+						var $successMessage = $('<p>')
+							.addClass('submit--message')
+							.css({ 'text-align': 'center' });
+						$form.after($successMessage);
+					}
+
+					$('.submit--message').html('Whoopee! ' + data.message);
 				})
 				.fail(function(data) {
-					console.log(data.message);
+					var response = JSON.parse(data.responseText);
+
 					$('.post__submit').val('Oops!');
+
+					if ($('.submit--message').length < 1) {
+						var $failMessage = $('<p>')
+							.addClass('submit--message')
+							.css({ 'text-align': 'center' });
+						$form.after($failMessage);
+					}
+
+					$('.submit--message').html('Unable to add blog post. ' + response.message);
 			});
 		}
 	}
