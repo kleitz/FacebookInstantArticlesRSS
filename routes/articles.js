9cd0f4c4f29ddce30fb4bfd47d34e2eb1df4ad21
Patrick Wilver <pwilver@hubspot.com>
2016-04-05 16:15:13 -0400

    Move all postgres queries to separate file, evaluate promise results here

--- routes/articles.js
+++ routes/articles.js
@@ -1,53 +1,28 @@
 var express = require('express');
 var router = express.Router();
-var request = require('request');
-var Q = require('q');
 
 var functions = require('../apiFunctions');
-
-var pg = require('pg').native;
-var PG_URL = process.env.DATABASE_URL || 'postgres://pwilver:@localhost/facebookia';
-var pgClient = new pg.Client(PG_URL);
-
-
-// Connect to db
-pgClient.connect(function(err) {
-	if (err) {
-		return console.error('could not connect to postgres', err);
-	} else {
-		pgClient.query("CREATE TABLE IF NOT EXISTS rssItems(id serial PRIMARY KEY NOT NULL,title text,canonical_url text,guid bigint,meta_description text,publish_date bigint,author text)", function(err, result) {
-			if (err) { 
-				return console.error('error creating table', err ); 
-			}
-		});
-
-		return console.log('Connected to postgres!');
-	}
-});
-
-
 
 
 // POST blog data to db
 // =====================
 router.post('/submit', function(req, res) {
-	var id = req.param('blog_id'),
-		postData;
+	var id = req.param('blog_id');
 
-	functions.fetchBlogData(id).then(function(data) {
-		postData = data.data[0];
-	});
-
-	var title = postData.name,
-		canonical_url = postData.url,
-		meta_description = postData.meta_description,
-		publish_date = postData.publish_date,
-		author = postData.blog_author.full_name;
-
-	// 	functions.postNewArticle(title, canonical_url, id, meta_description, publish_date, author).then(function(response) {
-	// 		res.status(response.status).json(response);
-	// 	});
-	// });
+	if (id) {
+		functions.fetchBlogData(id)
+			.then(function(responseData) {
+				return functions.postNewArticle(responseData);
+			}).then(function(responseObject) {
+				res.status(responseObject.status).json(responseObject);
+			}).catch(function(error) {
+				res.status(400).json(error);
+			}).done();
+	} else {
+		res.status(400).json({
+			message: 'Please add an ID in the text box above.'
+		});
+	}
 });
 
 module.exports = router;
